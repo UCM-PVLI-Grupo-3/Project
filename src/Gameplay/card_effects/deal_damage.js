b2863d65f73d98c0de5f94856882a533c1128096
Dekros <clearsky3210@gmail.com>
2024-11-08 17:51:00 +0100

    feat: add card effects placeholders

---
+++ src/Gameplay/card_effects/deal_damage.js
@@ -0,0 +1,15 @@
+import { CardEffect } from 'card_effect.js';
+
+class DealDamageEffect extends CardEffect {
+    constructor(amount_of_damage) {
+        super();
+        this.amount_of_damage = amount_of_damage;
+        
+    }
+
+    apply_effect() {
+        // TODO: implement effect application
+    }
+}
+
+export { DealDamage };
---
+++ src/Gameplay/card_effects/stun.js
@@ -0,0 +1,14 @@
+import { CardEffect } from 'card_effect.js';
+
+class StunEffect extends CardEffect {
+    constructor(stun_duration) {
+        super();
+        this.stun_duration = stun_duration;
+    }
+
+    apply_effect() {
+        // TODO: implement effect application
+    }
+}
+
+export { StunEffect };
