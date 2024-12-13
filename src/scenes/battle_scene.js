8706e980e9131c442b62bf96791508a9c6970240
ChichoRD <chichord2005@hotmail.com>
2024-12-13 01:25:54 +0100

    Revert "Merge branch 'main' of https://github.com/UCM-PVLI-Grupo-3/Project"
    This reverts commit bccf0ee94287e540d61898a51df27720150121f8, reversing
    changes made to a34be0a20fcef919be78a894407f03ef76f67c37.

--- src/scenes/battle_scene.js
+++ src/scenes/battle_scene.js
@@ -10,10 +10,8 @@ import { ActionSelectorRadioGroup } from "../gameplay/player_action_selection/ac
 import { CardHandActionFeature, SceneCardHandActionFeature } from "../gameplay/player_action_selection/action_features/card_hand_action_feature_sel.js";
 import { DiceChangeActionFeature, SceneDiceChangeActionFeature } from "../gameplay/player_action_selection/action_features/dice_change_action_feature_sel.js";
 import { Player } from "../gameplay/player.js";
-import { Health } from "../gameplay/health.js";
-import { Enemy,SceneEnemy } from "../Gameplay/enemy.js";
-import { EnemyTypeList } from "../Gameplay/EnemyTypeList.js";
-
+import { Health, Block } from "../gameplay/health.js";
+import { SceneDiceBox } from "../gameplay/dice_box.js";
 
 const BATTLE_SCENE_DEFAULT_SICE_SLOTS = 3;
 
@@ -77,10 +75,20 @@ class BattleScene extends Phaser.Scene {
      * */
     dice_change_button;
 
-      /**
-     * @type {Enemy}
+    /**
+     * @type {ActionSelectorRadioGroup}
+     */
+    card_hand_action_selector;
+
+    /**
+     * @type {Phaser.GameObjects.Sprite}
+     */
+    turn_execution_bell;
+
+    /**
+     * @type {SceneDiceBox}
      * */
-      Enemy;
+    dice_change_box;
 
     constructor() {
         super({ key: KEYS_SCENES.BATTLE });
@@ -97,35 +105,39 @@ class BattleScene extends Phaser.Scene {
     }
 
     create(data) {
-        this.scene_emotion_stack = this.add.existing(new SceneEmotionStack(this, 100, 100, [
-            EMOTION_TYPE.ANGER(),
-            EMOTION_TYPE.ANGER(),
-            EMOTION_TYPE.HAPPINESS(),
-            EMOTION_TYPE.HAPPINESS(),
-            EMOTION_TYPE.CALM()
-        ], 8));
-
         const screen_width = this.renderer.width;
         const screen_height = this.renderer.height;
 
         // TODO: populate
         this.attack_dice_slots = this.add.existing(new SceneDiceSlots(this, screen_width / 2 - 110, 450, 3, [
-            new SceneDice(this, 0, 0, DICE_TYPE.D4)
+            new SceneDice(this, 0, 0, DICE_TYPE.D6)
         ]));
         this.defence_dice_slots = this.add.existing(new SceneDiceSlots(this, screen_width / 2, 450, 3, [
             new SceneDice(this, 0, 0, DICE_TYPE.D4)
         ]));
         this.heal_dice_slots = this.add.existing(new SceneDiceSlots(this, screen_width / 2 + 110, 450, 3, [
-            new SceneDice(this, 0, 0, DICE_TYPE.D4)
+            new SceneDice(this, 0, 0, DICE_TYPE.D12)
         ]));
 
-        let dice_change_feature = new DiceChangeActionFeature(this, [this.attack_dice_slots, this.defence_dice_slots, this.heal_dice_slots]);
+        const emotion_stack_y = this.attack_dice_slots.y;
+
+        const emotion_stack_x_offset = -150;
+        const emotion_stack_x = this.attack_dice_slots.x + emotion_stack_x_offset;
+
+        this.scene_emotion_stack = this.add.existing(new SceneEmotionStack(this, emotion_stack_x, emotion_stack_y, [
+            
+        ], 7)).setScale(0.50);
+
+        this.dice_change_box = new SceneDiceBox(this, 80, 550, 4);
+        this.add.existing(this.dice_change_box);
+
+        let dice_change_feature = new DiceChangeActionFeature(this, [this.attack_dice_slots, this.defence_dice_slots, this.heal_dice_slots], this.dice_change_box);
         this.dice_change_button = new SceneDiceChangeActionFeature(this, 790, 400, dice_change_feature);
         this.add.existing(this.dice_change_button);
 
 
         const initial_cards_count = 6;
-        let initial_cards = [...GAMEPLAY_CARDS].sort(() => 0.5 - Math.random());//.slice(0, initial_cards_count);
+        let initial_cards = [...GAMEPLAY_CARDS].sort(() => 0.5 - Math.random()).slice(0, initial_cards_count);
         console.assert(initial_cards instanceof Array, "error: initial_cards must be an array");
         console.assert(initial_cards.length === initial_cards_count, "error: initial_cards.length !== initial_cards_count");
 
@@ -134,14 +146,16 @@ class BattleScene extends Phaser.Scene {
         let card_deck = new CardDeck(30, initial_cards);
 
         // SceneCardHands
-        this.attack_scene_card_hand = this.add.existing(new SceneCardHand(this, screen_width / 2, screen_height / 2 + 180, card_deck, 3, CARD_ACTION_TYPE.ATTACK));
-        this.defence_scene_card_hand = this.add.existing(new SceneCardHand(this, screen_width / 2, screen_height / 2 + 180, card_deck, 2, CARD_ACTION_TYPE.DEFENCE));
-        this.heal_scene_card_hand = this.add.existing(new SceneCardHand(this, screen_width / 2, screen_height / 2 + 180, card_deck, 2, CARD_ACTION_TYPE.HEAL));
+        this.attack_scene_card_hand = this.add.existing(new SceneCardHand(this, screen_width / 2, screen_height / 2 + 180, card_deck, 3, CARD_ACTION_TYPE.ATTACK)).setVisible(false);
+        this.defence_scene_card_hand = this.add.existing(new SceneCardHand(this, screen_width / 2, screen_height / 2 + 180, card_deck, 2, CARD_ACTION_TYPE.DEFENCE)).setVisible(false);
+        this.heal_scene_card_hand = this.add.existing(new SceneCardHand(this, screen_width / 2, screen_height / 2 + 180, card_deck, 2, CARD_ACTION_TYPE.HEAL)).setVisible(false);
         
         this.player = new Player(
             card_deck,
             this.attack_scene_card_hand,
-            new Health(12, 0, 12, (health) => { this.on_player_health_set(health); })
+            new Health(12, 0, 12, (health) => { this.on_player_health_set(health); }),
+            new Block(0, 0, 6, (block) => { this.on_player_block_set(block); }),
+            dice_change_feature.dice_slots_registers
         );
 
         // SceneCard selection
@@ -149,7 +163,13 @@ class BattleScene extends Phaser.Scene {
         let defence_card_hand_feature = new CardHandActionFeature(this.defence_scene_card_hand);
         let heal_card_hand_feature = new CardHandActionFeature(this.heal_scene_card_hand);
 
-        let action_selection_group = new ActionSelectorRadioGroup(this, [attack_card_hand_feature, defence_card_hand_feature, heal_card_hand_feature, dice_change_feature]);
+        let action_selection_group = new ActionSelectorRadioGroup(
+            this,
+            [attack_card_hand_feature, defence_card_hand_feature, heal_card_hand_feature, dice_change_feature],
+            false,
+            true
+        );
+        this.card_hand_action_selector = action_selection_group;
         
         this.attack_card_hand_button = this.add.existing(new SceneCardHandActionFeature(this, screen_width / 2 - 115 - 50, 260, attack_card_hand_feature));
         this.defence_card_hand_button = this.add.existing(new SceneCardHandActionFeature(this, screen_width / 2 - 50, 260, defence_card_hand_feature));
@@ -159,10 +179,23 @@ class BattleScene extends Phaser.Scene {
             this.attack_card_hand_button.feature_selector,
             this.defence_card_hand_button.feature_selector,
             this.heal_card_hand_button.feature_selector,
+        ], true, false);
 
-        ]);
-            this.Enemy = new SceneEnemy(this,1,1,1);
-        
+        //this.dice_change_button.y += 200;
+        const bell_x = this.dice_change_button.x;
+        const bell_y_offset = 150;
+        const bell_y = this.dice_change_button.y + this.dice_change_button.height + bell_y_offset;
+        this.turn_execution_bell = 
+            this.add.sprite(bell_x, bell_y, KEYS_ASSETS_SPRITES.TURN_EXECUTION_RING_BUTTON_RELEASE)
+            .setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, (ptr, local_x, local_y, event) => {
+                // TODO: play bell press anim
+
+                this.execute_turn();
+            }
+        ).setOrigin(0.0, 0.0).setTint(0xCCA049).setScale(0.75).setDepth(-1);
+        //this.turn_execution_bell.outl
+        console.log(action_selection_group);
+        console.log(card_hand_selection_group);
     }
 
     update(time_milliseconds, delta_time_milliseconds) {
@@ -174,6 +207,14 @@ class BattleScene extends Phaser.Scene {
 
     on_player_health_set(health) {
         this.events.emit(KEYS_EVENTS.PLAYER_HEALTH_SET, health);
+    }
+
+    on_player_block_set(block) {
+        this.events.emit(KEYS_EVENTS.PLAYER_BLOCK_SET, block);
+    }
+
+    execute_turn() {
+        this.player.execute_turn(this.card_hand_action_selector);
     }
 }
 
